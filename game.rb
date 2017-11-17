ff9d22aeee1f6afc91728b9f1c1d1e6c3e2120d0
LJ Nissen <lj.nissen@gmail.com>
2017-11-17 17:46:17 +0100

    Add code from gosu tutorial to game.rb

--- game.rb
+++ game.rb
@@ -1,3 +1,30 @@
+require 'gosu'
+
+class Game < Gosu::Window
+  def initialize
+    super 640, 480
+    self.caption = "Tutorial Game"
+  end
+  
+  def update
+    # ...
+  end
+  
+  def draw
+    # ...
+  end
+
+  def name
+    'Game'
+  end
+end
+
+Game.new.show
+
+
+
+
+
 class Game
 	def name
 		'Game'
