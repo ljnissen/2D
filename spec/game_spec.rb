57976d5f66dedb163763bc3422febaf07b9d15a8
LJ Nissen <lj.nissen@gmail.com>
2017-11-18 18:31:45 +0100

    Add rspec tests templates for all methods in game.rb

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,29 @@
 require 'spec_helper'
 
 describe "Game" do 
+	describe ".initialize" do
+		it "returns a Game object" do
+			expect(Game.new).to be_an_instance_of Game
+		end
+
+	end
+
 	describe ".name" do
 		it "returns a default name of 'Game'" do
 			expect(Game.new.name).to eq 'Game'
 		end
 	end
+
+
+
+	describe ".draw" do
+
+	end
+
+	describe ".update" do
+
+	end
+
+
 end
 	
