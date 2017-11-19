4b4b49d9eb30c326f5ac4374e682bfd29d3680cc
LJ Nissen <lj.nissen@gmail.com>
2017-11-19 16:34:07 +0100

    Setup project to start

--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -1,7 +1,7 @@
-require 'spec_helper'
+require_relative '../spec_helper'
 
 describe "Game" do 
-	describe ".initialize" do
+	describe ".new" do
 		it "returns a Game object" do
 			expect(Game.new).to be_an_instance_of Game
 
