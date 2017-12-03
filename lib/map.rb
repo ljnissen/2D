23ebb3bfbc2320dfbb7342e6ed135cd232ab0e6a
LJ Nissen <lj.nissen@gmail.com>
2017-12-03 18:10:26 +0100

    Draw generic tile all over game board

--- lib/map.rb
+++ lib/map.rb
@@ -5,21 +5,28 @@ class Map
 	TILEABLE			= true
 	DEFAULT_TILE_SIZE	= 32
 
-	attr_reader :tiles
+	attr_reader :tile_size
 
-	def initialize
-		@tiles = Gosu::Image.load_tiles Game.game_window,
-										file_path,
-										DEFAULT_TILE_SIZE,
-										DEFAULT_TILE_SIZE,
-										TILEABLE
+	def initialize tile_size = DEFAULT_TILE_SIZE
+		@tile_size = tile_size
 	end
 
 	def update
 	end
 
 	def draw
-		@tiles[0].draw 0, 0, 0
+		z_index = 0
+		tile = tiles[0]
+
+		columns.times do |x|
+			rows.times do |y|
+				draw_tile tile,
+					x * tile_size,
+					y * tile_size,
+					z_index
+			end
+		end
+
 	end
 
 	private
@@ -28,9 +35,30 @@ class Map
 		@file_path ||= [
 			TILE_SETS_PATH,
 			DEFAULT_TILE_SET].join('/') << EXTENSION
-	end		
+	end	
 
+	def z_index
+	end
 
+	def columns
+		columns = 10
+	end	
+
+	def rows
+		rows = 5
+	end
+
+	def tiles
+		@tiles ||= Gosu::Image.load_tiles Game.game_window,
+											file_path,
+											DEFAULT_TILE_SIZE,
+											DEFAULT_TILE_SIZE,
+											TILEABLE
+	end
+
+	def draw_tile tile, x, y, z
+		tile.draw x, y, z
+	end
 
 
 end
