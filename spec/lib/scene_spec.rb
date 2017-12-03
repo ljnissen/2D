137d6f8626a1199770416c304afc8b9b077b18a7
LJ Nissen <lj.nissen@gmail.com>
2017-12-03 17:30:18 +0100

    Add tests to scene_spec

--- spec/lib/scene_spec.rb
+++ spec/lib/scene_spec.rb
@@ -1,9 +1,14 @@
 require_relative '../spec_helper'
 
 describe Scene do
-	let(:scene) { Scene.new }
+	let(:game_window) 		{ GameWindow.new }
+	let(:scene) 			{ Scene.new }
 	
+	before { Game.game_window = game_window }
+
 	describe ".new" do
-	
+		it "has sprites" do
+			expect(scene.sprites).to_not be_empty
+		end
 	end	
 end
